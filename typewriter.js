9eeca6635a7fdcc1fc72ba95d64a8f67f08e6189
Roshanak Akbarifar <roshanak.akbarifar@gmail.com>
2021-06-28 20:19:36 +0000

    tiny lint

--- typewriter.js
+++ typewriter.js
@@ -1,9 +1,7 @@
 const typewriter = function() {
   const sentence = "hello there from lighthouse labs\n";
   let delay = 0;
-  let counter = 0;
   for (const char of sentence) {
-    counter++;
     delay += 50;
     setTimeout(() => {
       process.stdout.write(char);
